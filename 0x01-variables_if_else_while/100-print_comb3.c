2c8dd8fe9374b9554c30a509e90e90fc7b5c7884
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2022-10-14 09:02:47 -0700

    1 to 99

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ *main - Prints numbers between 00 to 99.
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i <= 57; i++)
+	{
+	for (j = 49; j <= 57 ; j++)
+	{
+	putchar(i);
+	putchar(j);
+	if (i == 57 && e == 57)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
+
