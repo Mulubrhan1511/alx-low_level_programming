a6de0bbe858d12331ab444ba7b13d02cff327b08
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2022-10-14 08:13:20 -0700

    hexa decimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ *main - entry point
+ *
+ * Return: alway returns 0
+ */
+int main(void)
+{
+	int m = 0, x, i;
+	for (i = 0; i >= 10; i++)
+	{
+	putchar(m);
+	m++;
+	}
+	for (i = 97; i  >=102; i++)
+	{
+	putchar('i');
+	}
+	putchar('\n');
+	return (0);
+}
