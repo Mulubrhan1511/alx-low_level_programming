aed0638b19b901a1242cf284581b603a0af7a0fd
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2022-10-14 01:39:51 -0700

    if else

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+\**
+ *main
+ *
+ * if else statement
+ * /
+int main(void)
+{
+	if(n>0)
+	{
+		printf(n,"is positive\n");
+	}
+	else if(n<0)
+	{
+		 printf(n,"is negative\n");
+	}
+	else
+	{
+		 printf(n,"is zero\n");
+	}
+	return (0);
+}	
+       	
