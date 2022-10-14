a4f87cc16c820e204cde08bd177cf78b09a4df6d
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2022-10-14 07:35:26 -0700

    assending to desanding

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ *main - Prints numbers between 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char m;
+
+	for (m = 'z'; m < a; m--)
+	{
+	putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
+
