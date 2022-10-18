0f993e095d1ad4e3bdceb4c3ee520c1b54ed2363
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2022-10-18 08:34:05 -0700

    mule

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *print_last_digit - Computes the absolute value
+ *
+ *@n: input number as an integer.
+ *Return: absolute v
+ **/
+int print_last_digit(int n)
+{
+	n = (n % 10);
+	return (n);	
+}
+
