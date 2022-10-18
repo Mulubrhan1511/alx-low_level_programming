c6373cab9c9de7c658feb0830deb4e727be9e28d
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2022-10-18 05:51:40 -0700

    many

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 /**
  *_islower - check lower case.
  *
- * parameter or member c if c is lower case it say c is lower case
+ * parameter - c if c is lower case it say c is lower case
  *Return: Always 0 (Success)
  */
 int _islower(int c)
