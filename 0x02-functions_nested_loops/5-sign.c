16f326dbe1b560572e3ef2f1ee38834643647634
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2022-10-18 06:47:26 -0700

    much

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_sign - Shows 1 if the input is a
+ *
+ *@c: The character in ASCII code
+ *Return: 1 for lowercase character. 0 for the rest.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	return (1);
+	else if (n = 0)
+	return (0);
+	else 
+	return (-1);
+}
