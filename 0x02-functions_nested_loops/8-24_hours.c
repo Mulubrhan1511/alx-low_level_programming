f9c022984c45ad1e731d8756c2dd6bcbe26c103e
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2022-10-18 09:45:49 -0700

    mule

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,10 +13,8 @@ void jack_bauer(void)
 	{
 	for (m = 48; m <= 57; m++)
 	{
-
 	for (o = 48; o <= 53; o++) 
 	{
-
 	for (p = 48; p <= 57; p++) 
 	{
 	if (n >= 50 && m >= 52)
@@ -29,7 +27,6 @@ void jack_bauer(void)
 	_putchar('\n');
 	}
 	}
-
 	}	
 	}
 }
