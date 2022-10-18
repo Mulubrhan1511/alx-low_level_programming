9f329df1ec666d15016a14bbba326e99bbb42a8a
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2022-10-18 05:20:25 -0700

    10  alphabate

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ *
+ *  * print_alphabet_x10 - Prints the alphabet in lowercase.
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+void print_alphabet_x10(void)
+{
+	int i, j;
+	for (i = 1; i <= 10; i++)
+	{
+	for (j = 97; j <= 122; j++)
+	{
+	_putchar(i);
+	}
+	_putchar('\n');
+	}
+	_putchar('\n');
+}
