34c9e5d826368503cb703d6d15d72dde69a1e8a0
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2022-10-18 09:10:40 -0700

    mule

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ *print_last_digit - Prints the last digit
+ *@n: input number as an integer.
+ *@m: is integer
+ *
+ *Return: last digit.
+ **/
+void jack_bauer(void)
+{
+	int n, m, o, p;
+	for (n = 48; n <= 50; n++)
+	{
+	for (m = 48; m <= 57; m++)
+	{
+	for (o = 48; o <= 53; o++) 
+	{
+	for (p = 48; p <= 57; p++) 
+	{
+	if (a >= 50 && b >= 52)
+	break;
+	_putchar(n);
+	_putchar(m);
+	_putchar(58);
+	_putchar(o);
+	_putchar(p);
+	_putchar('\n');
+	}
+	}
+	}	
+	}
+}
