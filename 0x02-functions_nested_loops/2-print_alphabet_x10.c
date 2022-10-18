9f0f099a48be85a6bde5f2b742bbc9320eeb79a0
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2022-10-18 05:32:56 -0700

    alphabate

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,13 @@
 #include "main.h"
 /**
+ *print_alphabet_x10 - Prints the alphabet in lowercase.
  *
- *  * print_alphabet_x10 - Prints the alphabet in lowercase.
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ *Return: Always 0 (Success)
+ */
 void print_alphabet_x10(void)
 {
 	int i, j;
+
 	for (i = 1; i <= 10; i++)
 	{
 	for (j = 97; j <= 122; j++)
