a4be55b8141ad6803010fea667de6e94d3601ae5
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2022-10-18 10:01:37 -0700

    mule

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,14 +11,12 @@ void jack_bauer(void)
 
 	for (n = 48; n <= 50; n++)
 	{
-	for (m = 48; m <= 57; m++)
-	{
-
-	for (o = 48; o <= 53; o++) 
-	{
-
-	for (p = 48; p <= 57; p++) 
-	{
+		for (m = 48; m <= 57; m++)
+		{
+			for (o = 48; o <= 53; o++) 
+			{
+				for (p = 48; p <= 57; p++) 
+				{
 	if (n >= 50 && m >= 52)
 	break;
 	_putchar(n);
@@ -27,9 +25,9 @@ void jack_bauer(void)
 	_putchar(o);
 	_putchar(p);
 	_putchar('\n');
-	}
-	}
-	}
+				}
+			}
+		}
 
 	}
 }
