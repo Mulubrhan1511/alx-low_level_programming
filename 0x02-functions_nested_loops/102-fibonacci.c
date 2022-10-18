a777a71edffba28003b1b8f2d8899a12f8de41c6
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2022-10-18 12:49:50 -0700

    mula

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,12 +6,13 @@
  */
 int main(void)
 {
+	int n;
 	long int l, m, fib;
 	
 	l = 1;
 	m = 2;
 	printf("%ld, %ld", l, m);
-	for (int n = 0; n < 48; n++)
+	for (n = 0; n < 48; n++)
 	{
 	fib = m + l;
 	printf(", %ld", fib);
