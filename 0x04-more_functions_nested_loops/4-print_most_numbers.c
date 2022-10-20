9befa450e87b200ab3c0634ef3dbfc8633d4817f
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2022-10-20 09:32:58 -0700

    mule

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- *print_most_numbers print 0 to 9 except 2 and 4.s
+ *print_most_numbers print 0 to 9 except 2 and 4
+ *(neither 2 nor 4).
  *Return: 1 if is an uppercase character, 0 in other case.
  */
 void print_most_numbers(void)
 {
 	int i;
-	for (i 48; i <= 57; i++)
+	for (i = 48; i <= 57; i++)
 	{
 	if (i != 50 && i != 52)
 	_putchar(i);
