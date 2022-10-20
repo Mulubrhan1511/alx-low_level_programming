d59b3e604509c5b51ebdfa0f0eb0cf9cb3617a9c
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2022-10-20 06:35:50 -0700

    mule

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include<stdlib.h>
+#include<time.h>
+
+/**
+ *_isupper - checks if parameter is an uppercase character.
+ *@c: input character.
+ *Return: 1 if is an uppercase character, 0 in other case.
+ */
+
+void print_numbers(void)
+{
+	for (i = 48; i <= 57; i++)
+	{
+	_putchar(i);
+	}
+}
