aaebd455fb36dce787f1050cc849a41483039d24
Mulubrhan1511 <113607083+Mulubrhan1511@users.noreply.github.com>
2022-10-20 13:27:43 -0700

    mule

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,32 +1,33 @@
 #include <stdio.h>"
 
 /**
- *main - Fizz buzz.
- *Return: 1 if is an uppercase character, 0 in other case.
+ * main - prints Buzz each numbers of 3 and 5.
+ * Return: Always 0.
  */
-
 int main(void)
 {
-	int m;
+	int n;
 
-	for (m = 1; m <= 100; m++)
+	n = 1;
+	printf("%d", n);
+	for (n = 2; n <= 100; n++)
 	{
-	if ((m % 3 == 0) && (m % 5 == 0))
-	{
-	printf(" FizzBuzz");
-	}
-	else if (m % 3 == 0)
-	{
-	printf(" Fizz");
-	}
-	else if (m % 5 == 0)
-	{
-	printf(" Buzz");
-	}
-	else
-	{
-	printf(" %d", m);
-	}
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf(" FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf(" Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf(" Buzz");
+		}
+		else
+		{
+			printf(" %d", n);
+		}
 	}
 	printf("\n");
 	return (0);
