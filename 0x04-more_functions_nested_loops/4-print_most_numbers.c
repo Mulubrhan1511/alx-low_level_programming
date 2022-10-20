9f17c6a4bb342cecdf4dd628784e7f996b7c8a4e
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2022-10-20 09:28:05 -0700

    mule

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *print_most_numbers print 0 to 9 except 2 and 4.
+ *print_most_numbers print 0 to 9 except 2 and 4.s
  *Return: 1 if is an uppercase character, 0 in other case.
  */
 void print_most_numbers(void)
