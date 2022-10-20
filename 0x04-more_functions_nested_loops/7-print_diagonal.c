ea76f302d85e4cc311d3b3accfadf77c7d2fca6c
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2022-10-20 11:37:00 -0700

    mule

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_diagonal - print 0 to 9 except 2 and 4.
+ *@n: given number
+ *Return: 1 if is an uppercase character, 0 in other case.
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 1; i <= n; i++)
+	{
+	for (j = 1; j > i; j++)
+	{
+	_putchar(' ');
+	}
+	_putchar(92);
+	_putchar('\n');
+	}
+}
