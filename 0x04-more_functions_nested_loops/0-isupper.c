94d280f9e0d4fb01c0b6218d3ef9563a97176d5e
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2022-10-20 05:44:43 -0700

    mule

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - entry point
+ *Return: always return 0
+ */
+int _isupper(int c)
+{
+	if (c >= 48 && c <= 98)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
+
