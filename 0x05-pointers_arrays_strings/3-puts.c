d43e3e86592a8ffff1543930c9f63b6b5e3926a0
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2022-10-24 06:28:34 -0700

    mule

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_strlen - size of string.
+ *@s: input char.
+ *Return: size of the s.
+ */
+void _puts(char *str)
+{
+	int m;
+
+	for (m = 0; *(s + m) != '\0'; m++)
+	{
+	_putchar(m);
+	}
+	_putchar('\n');
+}
