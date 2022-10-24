31c688e0d0d3a7e07507b10444e9443bb6bc2776
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2022-10-24 11:46:51 -0700

    mule

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *print_rev - prints a string, followed by a new line.
+ *@s: input string.
+ *Return: no return.
+ */
+void rev_string(char *s)
+{
+	int m;
+
+	for (m = 0; m >= 0; m++)
+	{
+	if (s[m] == '\0')
+	{
+	break;
+	}
+	}
+	for (m--; m >= 0; m--)
+	{
+	for (n = 0; n <= 0; n++)
+	{
+	s[n] = s[m];
+	}	
+	}
+}
