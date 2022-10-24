00ae971a81f50aab74fd0a06d513a1abce6e97dc
Mulubrhan1511 <113607083+Mulubrhan1511@users.noreply.github.com>
2022-10-24 04:49:49 -0700

    mule

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- *reset_to_98 - check the code
- *
- *@n:pointer 
- *Return: Always 98.
+ * reset_to_98 - takes a pointer to a parameter and updates its value.
+ * @n: input integer.
+ * Return: no return.
  */
 void reset_to_98(int *n)
 {
