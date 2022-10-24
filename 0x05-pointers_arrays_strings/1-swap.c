2368c424ce206d2104a2bb40fc7c0fa596b30f5a
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2022-10-24 04:59:50 -0700

    mule

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - takes a pointer to a parameter and updates its value.
+ *@a: input integer.
+ *@b: input integer.
+ *Return: swap of a and b.
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
