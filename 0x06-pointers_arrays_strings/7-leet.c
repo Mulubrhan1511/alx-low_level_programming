6c119057b4e80980476f85a0d4dd079500a7bf2d
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2022-10-27 06:02:31 -0700

    mule

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *leet - encodes a string into 1337
+ *@s: input string.
+ *Return: the pointer to dest.
+ */
+char *leet(char *s)
+{
+	int i, j;
+
+	char *m1 = "aAoOtTlL";
+	char *m2 = "44330071";
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	for (j = 0; j < 10; j++)
+	{
+	if (s[i] = m1[j])
+	{
+	s[i] = m2[j];
+	}
+	}
+	}
+	return (s);	
+}
