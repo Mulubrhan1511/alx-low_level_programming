4bd1ec1b797cad44dc0ad3ce388343826e638c6e
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2022-10-27 06:09:32 -0700

    mule

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,13 +9,13 @@ char *leet(char *s)
 {
 	int i, j;
 
-	char *m1 = "aAoOtTlL";
-	char *m2 = "44330071";
+	char m1[] = "aAoOtTlL";
+	char m2[] = "44330071";
 	for (i = 0; s[i] != '\0'; i++)
 	{
 	for (j = 0; j < 10; j++)
 	{
-	if (s[i] = m1[j])
+	if (s[i] == m1[j])
 	{
 	s[i] = m2[j];
 	}
