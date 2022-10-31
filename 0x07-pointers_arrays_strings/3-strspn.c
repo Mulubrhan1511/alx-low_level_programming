afcba84bf9ba28168853401ee816af738c837204
Mulubrhan1511 <113607083+Mulubrhan1511@users.noreply.github.com>
2022-10-31 10:28:23 -0700

    mule

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *_strspn - gets the length of a prefix substring.
- *@s: initial segment.
- *@accept: accepted bytes.
- *Return: the number of accepted bytes.
+ * _strspn - gets the length of a prefix substring.
+ * @s: initial segment.
+ * @accept: accepted bytes.
+ * Return: the number of accepted bytes.
  */
 unsigned int _strspn(char *s, char *accept)
 {
@@ -12,17 +12,17 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
-	bool = 1;
-	for (j = 0; *(accept + j) != '\0'; j++)
-	{
-	if (*(s + i) == *(accept + j))
-	{
-	bool = 0;
-	break; 
-	}
-	}
-	if (bool == 1)
-	break;
+		bool = 1;
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				bool = 0;
+				break;
+			}
+		}
+		if (bool == 1)
+			break;
 	}
 	return (i);
 }
