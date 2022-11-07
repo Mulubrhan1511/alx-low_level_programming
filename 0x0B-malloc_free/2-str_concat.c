713529ed0ecd7f94f96ff8997c41d17a6ad06eaf
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2022-11-07 05:56:21 -0800

    mule

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_strdup - duplicates string
+ *@str: string to copy
+ *Return: char to newly duplicated st
+ */
+char *str_concat(char *s1, char *s2)
+{
+	
+}
