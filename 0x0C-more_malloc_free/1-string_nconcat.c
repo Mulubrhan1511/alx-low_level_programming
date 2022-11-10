cb597b20e9987fda79529f63587f4217f5ad661b
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2022-11-10 04:48:01 -0800

    mu;le

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *string_nconcat - concats strings
+ *@s1: string one
+ *@s2: string two
+ *@n: n amount of bytes
+ *Return: return a char val
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	
+}
