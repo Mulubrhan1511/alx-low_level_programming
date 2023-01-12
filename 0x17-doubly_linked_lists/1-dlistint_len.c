36d5280cd65e2b843b4eb3c1304a559ec3fec1c5
Mulubrhan1511 <113607083+Mulubrhan1511@users.noreply.github.com>
2023-01-12 01:46:56 -0800

    mule

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,23 +1,29 @@
 #include "lists.h"
+
 /**
- *print_dlistint - prints all the elements of a
- *dlistint_t list
+ * dlistint_len - returns the number of elements in
+ * a double linked list
  *
- *@h: head of the list
- *Return: the number of nodes
+ * @h: head of the list
+ * Return: the number of nodes
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	int count = 0;
+	int count;
+
+	count = 0;
 
 	if (h == NULL)
 		return (count);
+
 	while (h->prev != NULL)
 		h = h->prev;
+
 	while (h != NULL)
 	{
 		count++;
 		h = h->next;
 	}
+
 	return (count);
 }
