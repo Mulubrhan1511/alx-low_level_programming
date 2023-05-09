cb99bd8b208f8808dbeb9d7a3a2326ffb5b47677
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-05-10 00:37:30 +0300

    mule

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,44 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 /**
- * binary_search - Searches for a value in a sorted array of integers using
- * @array: is a pointer to the first element of the array to search in
- * @size:  is the number of elements in array
- * @value: value is the value to search for
- * Return: -1 If value is not present in array or if array is NULL, otherwise
+ * binary_search - Searches for a value in a sorted array of integers using the Binary search algorithm.
+ * @array: A pointer to the first element of the array to search in.
+ * @size: The number of elements in array.
+ * @value: The value to search for.
+ *
+ * Return: The index of the value if found, or -1 if not found.
  */
+
 int binary_search(int *array, size_t size, int value)
 {
-size_t left = 0;
-size_t i;
-size_t right;
-int mid;
-right = size - 1;
-left = 0;
-if (array == NULL)
-{
-return (-1);
-}
-while (left <= right)
-{
-printf("Searching in array: ");
-for (i = left; i <= right; i++)
-{
-printf("%d ", array[i]);
-}
-printf("\n");
-mid = left + (right - left) / 2;
-if (array[mid] == value)
-{
-return (mid);
-}
-else if (array[mid] < value)
-{
-left = mid + 1;
-}
-else
-{
-right = mid - 1;
-}
-}
-return (-1);
+    int left = 0;
+    int right = size - 1;
+
+    if (array == NULL)
+        return (-1);
+
+    while (left <= right)
+    {
+        int mid = (left + right) / 2;
+
+        printf("Searching in array: ");
+        for (int i = left; i <= right; i++)
+            printf("%d%s", array[i], i == right ? "\n" : ", ");
+
+        if (array[mid] == value)
+            return (mid);
+        else if (array[mid] < value)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+
+    return (-1);
 }
